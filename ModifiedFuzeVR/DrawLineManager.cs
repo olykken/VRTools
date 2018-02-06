eae1cf09e0931e1eda4c47cf882a0069b576946a
Oliver Lykken <olykken@gmail.com>
2018-02-06 15:34:36 -0600

    Some modified files from FuzeVR tutorials

---
+++ ModifiedFuzeVR/DrawLineManager.cs
@@ -0,0 +1,40 @@
+﻿//Based on FusedVR
+//Modified by Oliver Lykken
+
+using UnityEngine;
+
+public class DrawLineManager : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+    public Material lineMaterial;
+
+    private MeshLineRenderer currLine;
+    private int numClicks = 0;
+	
+	void Update () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject lr = new GameObject();
+            lr.AddComponent<MeshFilter>();
+            lr.AddComponent<MeshRenderer>();
+            currLine = lr.AddComponent<MeshLineRenderer>();
+            currLine.lineMaterial = new Material(lineMaterial);
+            currLine.SetWidth(0.1f);   
+        } else if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            currLine.AddPoint(trackedObj.transform.position);
+            numClicks++;
+        }
+        else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            numClicks = 0;
+            currLine = null;
+        }
+
+            if (currLine != null)
+        {
+            currLine.lineMaterial.color = ColorManager.Instance.GetCurrentColor();
+        }
+	}
+}
---
+++ ModifiedFuzeVR/SimpleLineDrawer.cs
@@ -0,0 +1,34 @@
+﻿//Based on FusedVR
+//Modified by Oliver Lykken
+//Uses Steam Controller to draw by creating line renderer GameObjects, this is not very optimized
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLineDrawer : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+
+    private LineRenderer currLine;
+    private int numClicks = 0;
+
+    void Update()
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject lr = new GameObject();
+            currLine = lr.AddComponent<LineRenderer>();
+            currLine.startWidth=0.1f;
+            currLine.endWidth=0.1f;
+            numClicks = 0;
+        }
+        else if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            currLine.positionCount = numClicks + 1;
+            currLine.SetPosition(numClicks, trackedObj.transform.position);
+            numClicks++;
+        }
+    }
+}
