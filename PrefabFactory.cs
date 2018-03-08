04cc419b42ed41122d1b79f70e9271fdb55126e3
Oliver Lykken <olykken@gmail.com>
2018-03-08 13:10:32 -0600

    Update PrefabFactory.cs

--- PrefabFactory.cs
+++ PrefabFactory.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 
 public class PrefabFactory : EditorWindow {
 
-        [MenuItem("PGSproject/Create Prefab From Selected")]
+        [MenuItem("'YourProjectNameHere'/Create Prefab From Selected")]
         static void CreatePrefab()
         {
             GameObject[] objs = Selection.gameObjects;
